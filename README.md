ebae05f959d976a923ebe0e286ebfa5ee35e59cc
Samuel Schlemper <samuelschlemper2006@gmail.com>
2022-07-19 13:33:42 -0300

    Uma pequena mudança no README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mapa de Árvore
+Um mapa que mostra filmes que fizeram muito sucesso e suas bilheterias (sendo feito).
+[Link para o resultado](https://samuel-schlemper-schlemuel.github.io/Mapa_de_arvore/HTML.html)
--- README.mk
+++
@@ -1,3 +0,0 @@
-#Mapa de Árvore
-Um mapa que mostra filmes que fizeram muito sucesso e suas bilheterias (sendo feito).
-[Link para o resultado ](https://samuel-schlemper-schlemuel.github.io/Mapa_de_arvore/HTML.html)
